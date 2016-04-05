5d408a1de0ba2ad1598ead4a6571ee19655f5d51
RickyLo <wkloricky@gmail.com>
2016-04-06 02:39:34 +0800

    initial commit with crawler.java

---
+++ comp4321project/src/Crawler.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author RickyLo
+ */
+import java.util.Vector;
+import org.htmlparser.beans.StringBean;
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.AndFilter;
+import org.htmlparser.filters.NodeClassFilter;
+import org.htmlparser.tags.LinkTag;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+import java.util.StringTokenizer;
+import org.htmlparser.beans.LinkBean;
+import java.net.URL;
+import java.util.Enumeration;
+
+
+public class Crawler
+{
+	private String url;
+	Crawler(String _url)
+	{
+		url = _url;
+	}
+	public Vector<String> extractWords() throws ParserException
+
+	{
+            // extract words in url and return them
+            // use StringTokenizer to tokenize the result from StringBean
+            // ADD YOUR CODES HERE
+            StringBean sb = new StringBean ();
+            sb.setLinks (false);
+            sb.setReplaceNonBreakingSpaces (true);
+            sb.setCollapse (true);
+            sb.setURL (this.url); // the HTTP is performed here
+            String s = sb.getStrings ();
+//          System.out.println(s);
+            Vector<String> words = new Vector<String>(0);
+            StringTokenizer st = new StringTokenizer (s);
+            while(st.hasMoreTokens()) {
+                words.addElement(st.nextToken());       
+            }
+            return words;  
+	}
+	public Vector<String> extractLinks() throws ParserException
+
+	{
+            // extract links in url and return them
+            // ADD YOUR CODES HERE
+            Vector<String> links = new Vector<String>();
+            LinkBean linkBean = new LinkBean();  
+            linkBean.setURL(this.url);
+            String strUrlFilters=this.url;
+            URL[] urls = linkBean.getLinks();  
+            for (int i = 0; i < urls.length; i++) { 
+                String url=urls[i].toString();
+                String[] UrlFilters=strUrlFilters.trim().split(",");
+                for (String urlFilter : UrlFilters)
+                    if(url.startsWith(urlFilter)) links.add(url);
+            }
+            return links;
+	}
+	
+	public static void main (String[] args)
+	{
+            try
+            {
+                Crawler crawler = new Crawler("http://www.cse.ust.hk");
+
+
+//                Enumeration e=links.elements();
+//                while (e.hasMoreElements()) {         
+//                    System.out.println("link :" + e.nextElement());
+//                }  
+//                        Enumeration e=words.elements();
+//                        while (e.hasMoreElements()) {         
+//                            System.out.println("token :" + e.nextElement());
+//                        }  
+
+                Vector<String> words = crawler.extractWords();		
+
+                System.out.println("Words in "+crawler.url+":");
+                for(int i = 0; i < words.size(); i++)
+                        System.out.print(words.get(i)+" ");
+                System.out.println("\n\n");
+
+                Vector<String> links = crawler.extractLinks();
+                System.out.println("Links in "+crawler.url+":");
+                for(int i = 0; i < links.size(); i++)		
+                        System.out.println(links.get(i));
+                System.out.println("");
+
+            }
+            catch (ParserException e)
+            {
+                    e.printStackTrace ();
+            }
+
+	}
+}
